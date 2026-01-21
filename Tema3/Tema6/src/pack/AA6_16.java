6e90c8d5ee9da64de8dddc45a688ebd2d38a6419
TL47 <tlopdel0611@g.educaand.es>
2026-01-21 12:45:57 +0100

    minor change

--- Tema3/Tema6/src/pack/AA6_16.java
+++ Tema3/Tema6/src/pack/AA6_16.java
@@ -1,9 +1,17 @@
 package pack;
 
+import java.util.*;
+
 public class AA6_16 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * Lee una palabra o frase y muestra el proceso en el que cada letra se sustituye por otro
+		 * símbolo no alfabético. Por ejemplo el carácter 'a' se podría sustituir por el carácter '@', la
+		 * 'e' por '€', la 'i' por '1', etcétera.
+		 */
+		
+		
 
 	}
 
